8a1cde3ac6260d42f585b0b5c3deed74f81826f3
ayumu83s <ayumu.itou.1125@gmail>
2016-11-27 15:10:20 +0900

    経過時間を出してみた

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	float raptime = 0;
+	public UnityEngine.UI.Text raptimeLabel;
+	// Update is called once per frame
+	void Update () {
+		raptime += Time.deltaTime;
+		string minutes = Mathf.FloorToInt (raptime / 60).ToString("00");
+		string seconds = Mathf.FloorToInt (raptime % 60).ToString("00");
+		string nseconds = Mathf.FloorToInt ((raptime * 1000)  % 1000).ToString("00");
+		raptimeLabel.text = minutes + ":" + seconds + ":" + nseconds;
+	}
+}
