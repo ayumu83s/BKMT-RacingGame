8b1fe6e5a96f88c4f9c35c089e92d385721496d6
Yabuki <takoyaking411@gmail.com>
2017-02-19 20:57:37 +0900

    フォント追加など

---
+++ Assets/Scripts/ButtonPush.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonScript : MonoBehaviour
+{
+
+    public void ButtonPush()
+    {
+        Debug.Log("Button Push !!");
+    }
+}
